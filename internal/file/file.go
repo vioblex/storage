3bc95e79a9fe34a71cc3d320f6948f1c4546fcab
Nuriddin Muhitdinov <vioblex@gmail.com>
2021-10-17 12:19:08 +0500

    Add simple upload method

--- internal/file/file.go
+++ internal/file/file.go
@@ -1,6 +1,22 @@
 package file
 
+import "github.com/google/uuid"
+
 type File struct {
+	ID uuid.UUID
 	Name string
 	Data []byte
+}
+
+func NewFile(fileName string, blob []byte) (*File, error) {
+	fileID, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	return &File{
+		ID:   fileID,
+		Name: fileName,
+		Data: blob,
+	}, nil
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,13 @@
 package storage
 
+import "github.com/vioblex/storage/internal/file"
+
 type Storage struct {}
 
 func NewStorage() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(fileName string, blob []byte) (*file.File, error)  {
+	return file.NewFile(fileName, blob)
 }
